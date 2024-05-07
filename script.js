9abda1926ce42454fcb6a40b3de3f9ab11dee5c6
Alejandro <alvardoortizalejadrodario@gmail.com>
2024-05-07 09:11:23 -0600

    Se agregaron imagenes a las canciones , que se actualizan , y se actualizo el javascript

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 let audio = document.getElementById('audioPlayer');
-// Asegúrate de que el path incluya la carpeta 'Canciones/' antes del nombre del archivo
+let photo = document.querySelector('.photo');
 let playlist = [
     'Canciones/Alejandro Sanz - Corazon Partio [Latin Mix] (Official Music Video).mp3', 
     'Canciones/Chayanne - Dejaría Todo.mp3', 
     'Canciones/Marco Antonio Solís - Si No Te Hubieras Ido (Live).mp3',
     'Canciones/Maná - Oye mi amor En el Muelle de San Blas Clavado en un bar - Siempre en Domingo.mp3',
-    'Canciones/Te quiero.mp3'
+    'Canciones/Te quiero.mp3',
+    'Canciones/Ahora Te Puedes Marchar.mp3'
+];
+
+let images = [
+    'img/AlejandroSanz.jpeg',
+    'img/Chayanne.jpg',
+    'img/MAS.jpeg',
+    'img/Mana.jpeg',
+    'img/HG.jpeg',
+    'img/LM.jpeg'
 ];
 let currentSong = 0;
 
-audio.src = playlist[currentSong]; // Carga inicial de la primera canción
+audio.src = playlist[currentSong];
+photo.src = images[currentSong];
+
+function updateSongAndImage() {
+    audio.src = playlist[currentSong];
+    photo.src = images[currentSong];
+    audio.play();
+}
 
 function togglePlay() {
     if (audio.paused) {
@@ -21,12 +38,10 @@ function togglePlay() {
 
 function nextSong() {
     currentSong = (currentSong + 1) % playlist.length;
-    audio.src = playlist[currentSong];
-    audio.play();
+    updateSongAndImage();
 }
 
 function prevSong() {
     currentSong = (currentSong - 1 + playlist.length) % playlist.length;
-    audio.src = playlist[currentSong];
-    audio.play();
+    updateSongAndImage();
 }
